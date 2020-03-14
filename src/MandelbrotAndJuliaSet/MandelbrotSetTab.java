5fc3d611e5172a1565aad60f0eca54d9a13fd82f
Logophilist <ralf@van-aert.nl>
2020-03-14 15:39:26 +0100

    Custom Mandelbrotset

--- src/MandelbrotAndJuliaSet/MandelbrotSetTab.java
+++ src/MandelbrotAndJuliaSet/MandelbrotSetTab.java
@@ -8,12 +8,13 @@ import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
 import org.jfree.fx.FXGraphics2D;
 
 import java.awt.image.BufferedImage;
 
 //@TODO readTextFields(); Read every textField and return false if not right format.
-// @TODO tfError(); Error pop up for wrong textField formatting.
+//@TODO tfError(); Error pop up for wrong textField formatting.
 
 public class MandelbrotSetTab {
     private HBox hBox;
@@ -25,13 +26,17 @@ public class MandelbrotSetTab {
     private Label labelFocusR, labelFocusI, labelStepSize, labelIterations;
     private TextField tfFocusR, tfFocusI, tfStepSize, tfIterations;
 
+    private Stage popUp;
+    private Label labelError;
+    private String stringError;
+
     private JuliaSetLogic mandelbrotSetLogic;
     private BufferedImage renderIMG;
 
     public MandelbrotSetTab(){
         this.hBox = new HBox();
         this.vBox = new VBox();
-        this.canvas = new Canvas(1920, 980);
+        this.canvas = new Canvas(1920, 1080);
         this.g2d = new FXGraphics2D(this.canvas.getGraphicsContext2D());
 
         this.buttonRender = new Button("Render");
@@ -43,7 +48,7 @@ public class MandelbrotSetTab {
             }
         });
 
-        this.buttonReset = new Button("Reset");
+        this.buttonReset = new Button("Reset TextFields");
         this.buttonReset.setOnAction(event -> {
             this.setNormalMandelbrot();
         });
@@ -63,32 +68,46 @@ public class MandelbrotSetTab {
                 this.labelStepSize, this.tfStepSize,
                 this.labelIterations, this.tfIterations
         );
-
         this.setNormalMandelbrot();
-        this.render();
-
         this.vBox.getChildren().addAll(this.hBox, this.canvas);
+
+        this.popUp = new Stage();
+        VBox errorVBOX = new VBox();
+        this.labelError = new Label("Make sure every TextField is formatted right.\nEvery TextField should be a float.\nExcept Iterations, that should be float.\n");
+        errorVBOX.getChildren().add(this.labelError);
+        this.popUp.setScene(new Scene(errorVBOX));
     }
 
     private void tfError() {
     }
 
     private boolean readTextFields() {
-
-
+        try{
+            float focusR = Float.parseFloat(this.tfFocusR.getText());
+            float focusI = Float.parseFloat(this.tfFocusI.getText());
+            float stepSize = Float.parseFloat(this.tfStepSize.getText());
+            int iterations = Integer.parseInt(this.tfIterations.getText());
+            this.setMandelbrot(focusR, focusI, stepSize, iterations);
+        } catch (Exception e){
+            return false;
+        }
         return true;
     }
 
     private void render() {
         this.mandelbrotSetLogic.initMandlebrotSet();
         this.renderIMG = this.mandelbrotSetLogic.getImage();
+        this.g2d.clearRect(0,0, 1920, 1080);
         this.g2d.drawImage(this.renderIMG, null, null);
+    }
+    private void setMandelbrot(float focusR, float focusI, float stepSize, int iterations){
+        this.mandelbrotSetLogic = new JuliaSetLogic(1920, 1080, focusR, focusI, stepSize);
+        this.mandelbrotSetLogic.setMaxIterations(iterations);
     }
 
     private void setNormalMandelbrot(){
         this.tfFocusR.setText("0.0f"); this.tfFocusI.setText("0.0f"); this.tfStepSize.setText("0.002f"); this.tfIterations.setText("1000");
-        this.mandelbrotSetLogic = new JuliaSetLogic(1920, 1080, 0.0f, 0.0f, 0.002f);
-        this.mandelbrotSetLogic.setMaxIterations(1000);
+        this.setMandelbrot(0.0f, 0.0f, 0.002f, 1000);
     }
 
     public Node getNode(){ return this.vBox; }
