d99f949d72d785e92f9ef28d6f4bc0f83b5d4065
Logophilist <ralf@van-aert.nl>
2020-03-20 10:55:55 +0100

    Refactor Wave #4
    Commented TreeLine and TreeFractalLogic

--- src/Others/TreeFractalLogic.java
+++ src/Others/TreeFractalLogic.java
@@ -7,13 +7,35 @@ import java.util.ArrayList;
 public class TreeFractalLogic {
     private ArrayList<TreeLine> treeLines;
 
+    /**
+     * TreeFractalLogic Constructor
+     * creates treeLines ArrayList
+     */
+
     public TreeFractalLogic(){
         this.treeLines = new ArrayList<>();
     }
 
+    /**
+     * getTreeLines
+     * returns ArrayList of TreeLines.
+     * @return
+     */
+
     public ArrayList<TreeLine> getTreeLines() {
         return this.treeLines;
     }
+
+    /**
+     * displayTree
+     * recursively adds TreeLines to the ArrayList.
+     * @param order
+     * @param origin
+     * @param lengthDiff
+     * @param degree
+     * @param rightDiff
+     * @param leftDiff
+     */
 
     public void displayTree(int order, Point2D origin, double lengthDiff, double degree, double rightDiff, double leftDiff){
         Point2D nextOrigin = new Point2D(origin.getX() + 100 * Math.cos(degree) * lengthDiff, origin.getY() + 100 * Math.sin(degree) * lengthDiff);
--- src/Others/TreeLine.java
+++ src/Others/TreeLine.java
@@ -4,12 +4,19 @@ import javafx.geometry.Point2D;
 import org.jfree.fx.FXGraphics2D;
 
 import java.awt.*;
-import java.awt.geom.AffineTransform;
 
 public class TreeLine {
     private Point2D p1;
     private Point2D p2;
     private BasicStroke stroke;
+
+    /**
+     * TreeLine Constructor
+     * sets 2 Point2D's to create line with stroke width.
+     * @param p1
+     * @param p2
+     * @param stroke
+     */
 
     public TreeLine(Point2D p1, Point2D p2, float stroke){
         this.p1 = p1;
@@ -18,10 +25,24 @@ public class TreeLine {
     }
 
 
+    /**
+     * draw
+     * draws line on FXGraphics2D object with correct translations for OtherFractals.
+     * @param g2d
+     */
+
     public void draw(FXGraphics2D g2d){
         g2d.setStroke(this.stroke);
         g2d.drawLine((int)p1.getX(), (int)p1.getY() * -1 + 540, (int)p2.getX(), (int)p2.getY() * -1 + 540);
     }
+
+
+    /**
+     * draw
+     * draws line on Graphics2D object with correct translations for the image.
+     * Does NOT take in CameraTransform.
+     * @param g
+     */
 
     public void draw(Graphics2D g){
         g.setStroke(this.stroke);
